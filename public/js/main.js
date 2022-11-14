7ab2064cd8960e14fb784ae424e06bb6b86d0b0b
vladnomad <vlad.nmd@gmail.com>
2022-11-14 14:57:51 +0100

    Implementing JS classes to render menu cards

--- public/js/main.js
+++ public/js/main.js
@@ -134,4 +134,94 @@ window.addEventListener("DOMContentLoaded", () => {
             closeModal();
         }
     });
+
+// -------------------------- Cards -------------------------- //
+
+    class MenuCard {
+        constructor(num, alt, title, descr, price, parentSelector) {
+            this.num = num;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.rate = 4.55;
+            this.currencyExchange();
+        }
+
+        currencyExchange() {
+            this.price = Math.floor(this.price / this.rate) - 0.01;
+        }
+
+        render() {
+            const element = document.createElement("div");
+            element.innerHTML = `
+                <article aria-labelledby="menu__item--label-${this.num}" 
+                        class="menu__item"
+                        id="menu__item--${this.num}">
+                    <img src="img/menu/menu__1920--${this.num}.jpg" 
+                        alt=${this.alt} 
+                        srcset="img/menu/menu__428--${this.num}.jpg 338w, 
+                                img/menu/menu__810--${this.num}.jpg 427w, 
+                                img/menu/menu__1366--${this.num}.jpg 350w, 
+                                img/menu/menu__1920--${this.num}.jpg 420w, 
+                                img/menu/menu__2040--${this.num}.jpg 520w, 
+                                img/menu/menu__2550--${this.num}.jpg 620w, 
+                                img/menu/menu__3800--${this.num}.jpg 720w"
+                        sizes="(max-width: 429px) 338px, 
+                                (max-width: 811px) 427px, 
+                                (max-width: 1367px) 350px, 
+                                (max-width: 1921px) 420px, 
+                                (max-width: 2041px) 520px, 
+                                (max-width: 2551px) 620px, 
+                                720px" />
+                    <div class="bg__shadow absolute"></div>
+                    <h3 id="menu__item--label-${this.num}">
+                        ${this.title}&nbsp;&nbsp;menu
+                    </h3>
+                    <p class="menu__item--text">
+                        ${this.descr}
+                    </p>
+                    <hr>
+                    <div class="menu__item--price flex justify-between align-center">
+                        <p>
+                            Price:
+                        </p>
+                        <div class="menu__item--price-total">
+                            <span>${this.price}</span>
+                                USD/day
+                        </div>
+                    </div>
+                </article>
+            `;
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCard(
+        "1",
+        "Assorted delicious cheese and nuts, bruschette with yogurt, pesto and bresaola, and a plate of spaghetti on the marble table",
+        "FITNESS",
+        "The Fitness menu is a new approach to cooking - more fresh vegetables and fruits. For people who are into sports, being active and healthy. This is a completely new product, providing high quality food for a good price!",
+        80,
+        "#menu__wrapper .container"
+    ).render();
+
+    new MenuCard(
+        "2",
+        "Assorted delicious grilled seafood, meat and fruits, served with multiple sauces and a bottle of wine on the wooden table",
+        "PREMIUM",
+        "In the premium menu, we use not only beautiful packaging design, but also 'haute cuisine' cooking techniques and ingredients. Red fish, seafood, fruits - a dinner at the restaurant without going out!",
+        105,
+        "#menu__wrapper .container"
+    ).render();
+
+    new MenuCard(
+        "3",
+        "Indonesian vegan plate of vegetables cut in small pieces with quinoa and komosa served with sauce",
+        "VEGAN",
+        "Our special Vegan menu has a thorough selection of ingredients - a plant-based diet avoiding all animal foods such as meat, dairy, eggs and honey. Complete harmony with oneself and nature in every element!",
+        90,
+        "#menu__wrapper .container"
+    ).render();
 });
