b4aa0e74eca6a51a799469fa66eded388912ca8e
vladnomad <vlad.nmd@gmail.com>
2022-11-18 18:53:59 +0100

    Implementing ability to send data from forms to the server (using XAMMP on Linux) in JSON format

--- public/js/main.js
+++ public/js/main.js
@@ -236,4 +236,59 @@ window.addEventListener("DOMContentLoaded", () => {
         "menu__item",
         "num--3"
     ).render();
+
+// -------------------------- Server -------------------------- //
+
+    // Forms
+
+    const forms = document.querySelectorAll("form");
+
+    const message = {
+        load: "Please wait, loading...",
+        success: "Thank you! We will get in touch soon",
+        error: "Something went wrong..."
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement("div");
+            statusMessage.classList.add("status");
+            statusMessage.textContent = message.load;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+
+            request.open("POST", "server.php");
+            request.setRequestHeader("Content-type", "application/json");
+
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(value, key) {
+                object[key] = value;    
+            });
+
+            const json = JSON.stringify(object);
+            request.send(json);
+
+            request.addEventListener("load", () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 1500);
+                } else {
+                    statusMessage.textContent = message.error;
+                }
+            });
+        });
+    }
 });
