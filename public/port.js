38ef197f26eb777dd507ae71589296649a084963
vladnomad <vlad.nmd@gmail.com>
2023-02-13 19:33:20 +0100

    test port.js

--- public/port.js
+++ public/port.js
@@ -3,17 +3,16 @@ require('dotenv').config();
 const http = require('http');
 
 const port = parseInt(process.env.PORT, 10) || 4242;
-const name = process.env.MYNAME || "User";
 
 console.log(port);
 
 http.createServer((request, response) => {
-    response.writeHead(200, {
+/*     response.writeHead(200, {
         'Content-Type': 'text/plain'
     });
 
     response.write('Hello, ' + name + '!');
 
-    response.end();
+    response.end(); */
 
 }).listen(port);
