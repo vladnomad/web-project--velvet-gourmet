cdb414795e1bd3da8e5f5982644a7924eb5e172a
vladnomad <vlad.nmd@gmail.com>
2023-02-13 19:43:56 +0100

    test port.js

--- public/port.js
+++ public/port.js
@@ -3,16 +3,17 @@ require('dotenv').config();
 const http = require('http');
 
 const port = parseInt(process.env.PORT, 10) || 4242;
+const name = process.env.MYNAME || "User";
 
 console.log(port);
 
 http.createServer((request, response) => {
-/*     response.writeHead(200, {
+    response.writeHead(200, {
         'Content-Type': 'text/plain'
     });
 
-    response.write('Hello, ' + name + '!');
+    response.write(`Server is running at port ${port}`);
 
-    response.end(); */
+    response.end();
 
 }).listen(port);
