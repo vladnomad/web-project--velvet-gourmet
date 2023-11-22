0f4de2ea7bf28b98bab48d19c9ccf0597d1fcf06
vladnomad <vlad.nmd@gmail.com>
2023-11-22 13:07:30 +0100

    Automating Timer + Minor CSS fixes

--- src/js/index.js
+++ src/js/index.js
@@ -8,8 +8,23 @@ import slider from "./modules/slider";
 import calc   from "./modules/calc";
 
 window.addEventListener("DOMContentLoaded", () => {
+    const currentDate = new Date();
+    const nextMonth = new Date(currentDate);
+    const nextMonthSpan = document.querySelector(".next-month");
+    
+    nextMonthSpan.innerHTML = "1st of " + nextMonth.toLocaleDateString("en-US", { month: "long" })
+    nextMonth.setMonth(currentDate.getMonth() + 1);
+    nextMonth.setDate(1);
+
+    const lastDayOfCurrentMonth = new Date(nextMonth);
+
+    lastDayOfCurrentMonth.setDate(nextMonth.getDate() - 1);
+    lastDayOfCurrentMonth.setHours(0, 0, 0, 0);
+
+    const deadline = lastDayOfCurrentMonth.toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" }) + " " + lastDayOfCurrentMonth.toTimeString().split(" ")[0] + " GMT+0100";
+
     tabs(".tab", ".tabpanel", ".tablist", "js-active");
-    timer("timer__wrapper", "August 26 2023 00:00:00 GMT+0100");
+    timer("timer__wrapper", deadline);
     modal("[data-modal]", "modal");
     cards();
     server("form");
